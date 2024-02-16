e4eaa144fc6402b72545c8c3936fbe7f293441d4
Igor Cherkashov <cherkashov270999@gmail.com>
2024-02-16 23:07:11 +0300

    Task 3

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,34 +8,38 @@ parser.add_argument('first_file', type=str)
 parser.add_argument('second_file', type=str)
 parser.add_argument('-f', '--format', type=str, default='FORMAT', help='set format of output')
 args = parser.parse_args()
-#print(args.accumulate(args.integers))
+
+
+def load_file(file):
+    with open(file, "r") as read_file:
+        data = json.load(read_file)
+        return data
 
 
 def generate_diff(first_path, second_path):
-    with json.load(open(first_path)) as first_file_1:
-        with json.load(open(second_path)) as second_file_2:
-            #set_file_1 = set(first_file_1.keys()) - set(second_file_2.keys())
-            #set_file_2 = set(second_file_2.keys()) - set(first_file_1.keys())
-            #set_file_12 = set(first_file_1.keys()) & set(second_file_2.keys())
-            #text = []
-            #if set_file_1:
-            #    for key in set_file_1:
-            #        text.append('- ' + str(key) + ': ' + str(first_file_1[key]))
-            #if set_file_2:
-            #    for key in set_file_2:
-            #        text.append('+ ' + str(key) + ': ' + str(second_file_2[key]))
-            #if set_file_12:
-            #    for key in set_file_12:
-            #        if first_file_1[key] != second_file_2[key] and first_file_1 and second_file_2:
-            #            text.append('- ' + str(key) + ': ' + str(first_file_1[key]))
-            #            text.append('+ ' + str(key) + ': ' + str(second_file_2[key]))
-            #        else:
-            #            text.append('  ' + str(key) + ': ' + str(second_file_2[key]))
-            #text_sorted = sorted(text, key=lambda x: x[2])
-            #text_sorted.insert(0, '{')
-            #text_sorted.append('}')
-            #return '\n'.join(text_sorted)
-            return first_file_1.readlines()
+    first_file_1 = load_file(first_path)
+    second_file_2 = load_file(second_path)
+    set_file_1 = set(first_file_1.keys()) - set(second_file_2.keys())
+    set_file_2 = set(second_file_2.keys()) - set(first_file_1.keys())
+    set_file_12 = set(first_file_1.keys()) & set(second_file_2.keys())
+    text = []
+    if set_file_1:
+        for key in set_file_1:
+            text.append('- ' + str(key) + ': ' + str(first_file_1[key]))
+    if set_file_2:
+        for key in set_file_2:
+            text.append('+ ' + str(key) + ': ' + str(second_file_2[key]))
+    if set_file_12:
+        for key in set_file_12:
+            if first_file_1[key] != second_file_2[key] and first_file_1 and second_file_2:
+                text.append('- ' + str(key) + ': ' + str(first_file_1[key]))
+                text.append('+ ' + str(key) + ': ' + str(second_file_2[key]))
+            else:
+                text.append('  ' + str(key) + ': ' + str(second_file_2[key]))
+    text_sorted = sorted(text, key=lambda x: x[2])
+    text_sorted.insert(0, '{')
+    text_sorted.append('}')
+    return print('\n'.join(text_sorted))
 
 
 if __name__ == '__main__':
