7a168a961f59205c6ae55ef056b5cdb826dc1563
Igor Cherkashov <cherkashov270999@gmail.com>
2024-06-19 18:07:24 +0300

    Project 2

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from gendiff.scripts.gendiff import generate_diff
 from gendiff.scripts.parsing import load_file
+import json
 
 
 def test_generate_diff():
@@ -9,11 +10,14 @@ def test_generate_diff():
     file1_yaml = load_file("tests/fixtures/yaml/file1.yaml")
     file2_yaml = load_file("tests/fixtures/yaml/file2.yaml")
     with open("tests/fixtures/test_answer_stylish.txt", "r") as test_answer:
-        assert generate_diff(file1_json, file2_json, format = 'stylish') == test_answer.read()
-        assert generate_diff(file1_yaml, file2_yaml, format = 'stylish') == test_answer.read()
+        answer = test_answer.read()[:-1]
+        assert generate_diff(file1_json, file2_json, format = 'stylish') == answer
+        assert generate_diff(file1_yaml, file2_yaml, format = 'stylish') == answer
     with open("tests/fixtures/test_answer_plain.txt", "r") as test_answer:
-        assert generate_diff(file1_json, file2_json, format = 'plain') == test_answer.read()
-        assert generate_diff(file1_yaml, file2_yaml, format = 'plain') == test_answer.read()
+        answer = test_answer.read()[:-1]
+        assert generate_diff(file1_json, file2_json, format = 'plain') == answer
+        assert generate_diff(file1_yaml, file2_yaml, format = 'plain') == answer
     with open("tests/fixtures/test_answer_json.txt", "r") as test_answer:
-        assert generate_diff(file1_json, file2_json, format = 'json') == test_answer.read()
-        assert generate_diff(file1_yaml, file2_yaml, format = 'json') == test_answer.read()
+        answer = json.loads(test_answer.read()[:-1])
+        assert json.loads(generate_diff(file1_json, file2_json, format = 'json')) == answer
+        assert json.loads(generate_diff(file1_yaml, file2_yaml, format = 'json')) == answer
