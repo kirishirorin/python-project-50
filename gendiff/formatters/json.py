6515961ab12b7d66a7464381348cfc80a8906947
Igor Cherkashov <cherkashov270999@gmail.com>
2024-07-16 20:09:40 +0300

    changes #2

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -2,4 +2,8 @@ import json
 
 
 def json_f(diff):
-    return json.dumps(diff)
+    sorted_list = sorted(diff.items())
+    sorted_diff = {}
+    for key, value in sorted_list:
+        sorted_diff[key] = value
+    return json.dumps(sorted_diff)
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,15 +1,51 @@
-#!/usr/bin/env python3
 from gendiff.scripts.gendiff import generate_diff
 import json
 import pytest
 
 
-@pytest.mark.parametrize("file1,file2,path,format", [("tests/fixtures/json/file1.json", "tests/fixtures/json/file2.json", "tests/fixtures/test_answer_stylish.txt", "stylish"),
-                                                     ("tests/fixtures/yaml/file1.yaml", "tests/fixtures/yaml/file2.yaml", "tests/fixtures/test_answer_stylish.txt", "stylish"),
-                                                     ("tests/fixtures/json/file1.json", "tests/fixtures/json/file2.json", "tests/fixtures/test_answer_plain.txt", "plain"),
-                                                     ("tests/fixtures/yaml/file1.yaml", "tests/fixtures/yaml/file2.yaml", "tests/fixtures/test_answer_plain.txt", "plain"),
-                                                     ("tests/fixtures/json/file1.json", "tests/fixtures/json/file2.json", "tests/fixtures/test_answer_json.txt", "json"),
-                                                     ("tests/fixtures/yaml/file1.yaml", "tests/fixtures/yaml/file2.yaml", "tests/fixtures/test_answer_json.txt", "json")])
+JSON_PATH = 'tests/fixtures/json'
+YAML_PATH = 'tests/fixtures/yaml'
+@pytest.mark.parametrize(
+    "file1,file2,path,format",
+    [
+        (
+            f"{JSON_PATH}/file1.json",
+            f"{JSON_PATH}/file2.json",
+            "tests/fixtures/test_answer_stylish.txt",
+            "stylish"
+        ),
+        (
+            f"{YAML_PATH}/file1.yaml",
+            f"{YAML_PATH}/file2.yaml",
+            "tests/fixtures/test_answer_stylish.txt",
+            "stylish"
+        ),
+        (
+            f"{JSON_PATH}/file1.json",
+            f"{JSON_PATH}/file2.json",
+            "tests/fixtures/test_answer_plain.txt",
+            "plain"
+        ),
+        (
+            f"{YAML_PATH}/file1.yaml",
+            f"{YAML_PATH}/file2.yaml",
+            "tests/fixtures/test_answer_plain.txt",
+            "plain"
+        ),
+        (
+            f"{JSON_PATH}/file1.json",
+            f"{JSON_PATH}/file2.json",
+            "tests/fixtures/test_answer_json.txt",
+            "json"
+        ),
+        (
+            f"{YAML_PATH}/file1.yaml",
+            f"{YAML_PATH}/file2.yaml",
+            "tests/fixtures/test_answer_json.txt",
+            "json"
+        )
+    ]
+)
 def test_generate_diff(file1, file2, path, format):
     with open(path, "r") as test_answer:
         if format != "json":
